c3dd1deb5426471d371fb411cd90285eaab3c13b
mikezano <mikezano@gmail.com>
2022-02-05 14:06:44 -0700

    ily cakes

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="title"> Ily Cakes</h1>
+        <p className="cake">🎂</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
