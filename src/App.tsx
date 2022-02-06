e80c22229d23a961efb77116c797324e67f7a0dd
mikezano <mikezano@gmail.com>
2022-02-06 13:44:42 -0700

    pics

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {ImgGallery} from "./components/ImgGallery";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <header className="App-header">
         <h1 className="title"> Ily Cakes</h1>
         <p className="cake">🎂</p>
+        <ImgGallery />
       </header>
     </div>
   );
---
+++ src/components/ImgGallery.tsx
@@ -0,0 +1,13 @@
+
+export const ImgGallery = ()=>{
+    return <div className="gallery">
+        <img className="gallery__img" src={require('../img/cake7.jpg')}/>
+        <img className="gallery__img" src={require('../img/cake8.jpg')}/>
+        <img className="gallery__img" src={require('../img/cake1.jpg')}/>
+        <img className="gallery__img" src={require('../img/cake2.jpg')}/>
+        <img className="gallery__img" src={require('../img/cake3.jpg')}/>
+        <img className="gallery__img" src={require('../img/cake5.jpg')}/>
+        <img className="gallery__img" src={require('../img/cake6.jpg')}/>
+
+    </div>
+}
